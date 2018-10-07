0b9a11186c01083c72aeca39a1cbb090c670e549
Fernando Donini <fernando.donini@kobe.io>
2018-10-07 01:58:02 -0300

    # ViewMask With Clamped Values on Sin (Time)

---
+++ game-jam/Assets/Resources/Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public float moveSpeed = 6;
+
+	Rigidbody rigidbody;
+	Camera viewCamera;
+	Vector3 velocity;
+    public float velocityMagnitude;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		viewCamera = Camera.main;
+        velocityMagnitude = Vector3.Magnitude(velocity);
+
+    }
+
+	void Update () {
+		//Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+		//transform.LookAt (mousePos + Vector3.up * transform.position.y);
+		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * moveSpeed;
+
+        velocityMagnitude = Vector3.Magnitude(velocity);
+	}
+
+	void FixedUpdate() {
+		rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+}
